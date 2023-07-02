c0d6010a984100f08c98ba508a645476bbe3c804
JoseAl05 <jparancibialinker@gmail.com>
2023-07-02 00:46:38 -0400

    created navbar and navbar items

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,33 @@
+import { UserButton, auth } from '@clerk/nextjs';
+import { NavItems } from './NavItems';
+import StoreSwitcher from './StoreSwitcher';
+import prismadb from '@/lib/prismadb';
+
+const Navbar = async() => {
+
+    const { userId } = auth();
+
+    if(!userId){
+        redirect('/sign-in');
+    }
+
+    const stores = await prismadb.store.findMany({
+        where:{
+            userId
+        }
+    });
+
+    return (
+        <div className='border-b'>
+            <div className='flex h-16 items-center px-14'>
+                <StoreSwitcher items={stores}/>
+                <NavItems className='mx-6' />
+                <div className='ml-auto flex items-center space-x-4'>
+                    <UserButton afterSignOutUrl='/' />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;

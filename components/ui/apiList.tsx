d8a22e09efbe28e790f22112440439cb1725001c
JoseAl05 <jparancibialinker@gmail.com>
2023-07-02 23:58:59 -0400

    create api ui components

---
+++ components/ui/apiList.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { useOrigin } from '@/app/hooks/useOrigin';
+import { useParams } from 'next/navigation';
+import ApiAlert from './apiAlert';
+
+interface ApiListProps {
+    entityName: string;
+    entityIdName: string;
+}
+
+const ApiList: React.FC<ApiListProps> = ({ entityName, entityIdName }) => {
+
+    const params = useParams();
+    const origin = useOrigin();
+
+    const baseUrl = `${origin}/api/${params.storeId}`;
+
+    return (
+        <>
+            <ApiAlert
+                title='GET'
+                variant='public'
+                description={`${baseUrl}/${entityName}`}
+            />
+            <ApiAlert
+                title='GET'
+                variant='public'
+                description={`${baseUrl}/${entityName}/{${entityIdName}}`}
+            />
+            <ApiAlert
+                title='POST'
+                variant='admin'
+                description={`${baseUrl}/${entityName}`}
+            />
+            <ApiAlert
+                title='PATCH'
+                variant='admin'
+                description={`${baseUrl}/${entityName}/{${entityIdName}}`}
+            />
+            <ApiAlert
+                title='PATCH'
+                variant='admin'
+                description={`${baseUrl}/${entityName}/{${entityIdName}}`}
+            />
+        </>
+    );
+}
+
+export default ApiList;

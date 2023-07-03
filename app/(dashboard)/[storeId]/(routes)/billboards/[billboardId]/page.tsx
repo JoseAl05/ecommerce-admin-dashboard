ccf900d570cd4ddeb866f5af4857eb229d1b1fde
JoseAl05 <jparancibialinker@gmail.com>
2023-07-03 00:02:40 -0400

    created billboard and billboard form page

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -0,0 +1,25 @@
+import BillboardForm from '@/components/billboards/BillboardsForm';
+import prismadb from '@/lib/prismadb';
+
+const BillboardPageForm = async({
+    params
+}:{
+    params: { billboardId: string }
+}) => {
+
+    const billboard = await prismadb.billboard.findUnique({
+        where:{
+            id: params.billboardId
+        }
+    })
+
+    return (
+        <div className='flex-col'>
+            <div className='flex-1 space-y-4 p-8 pt-6'>
+                <BillboardForm billboard={billboard}/>
+            </div>
+        </div>
+    );
+}
+
+export default BillboardPageForm;

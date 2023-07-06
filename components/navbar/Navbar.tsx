7012983cbeb211c167d8a50b746eb1cd8306b603
JoseAl05 <jparancibialinker@gmail.com>
2023-07-06 12:43:34 -0400

    added theme toggle to change mode in navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,9 @@
+import { redirect } from 'next/navigation';
 import { UserButton, auth } from '@clerk/nextjs';
 import { NavItems } from './NavItems';
 import StoreSwitcher from './StoreSwitcher';
 import prismadb from '@/lib/prismadb';
+import { ThemeToggle } from '../theme/ThemeToggle';
 
 const Navbar = async() => {
 
@@ -23,6 +25,7 @@ const Navbar = async() => {
                 <StoreSwitcher items={stores}/>
                 <NavItems className='mx-6' />
                 <div className='ml-auto flex items-center space-x-4'>
+                    <ThemeToggle />
                     <UserButton afterSignOutUrl='/' />
                 </div>
             </div>
